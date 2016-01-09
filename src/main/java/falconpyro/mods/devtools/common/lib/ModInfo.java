343963a62b72f28f2f4561feebd9e058be0350fb
FalconPyro <josh.char.mico@googlemail.com>
2016-01-09 18:58:28 +0000

    Removed DevTools from source and replaced with DevTools binary

--- src/main/java/falconpyro/mods/devtools/common/lib/ModInfo.java
+++
@@ -1,9 +0,0 @@
-package falconpyro.mods.devtools.common.lib;
-
-/**
- * Created by falcon on 06/01/16.
- */
-public class ModInfo {
-    public static final String MODID = "FalconDevTools";
-    public static final String VERSION = "1.0";
-}
